8327f852214b21a4d53f5dc67a600bad2ccdf198
Manoj Fernando <manojf@99x.lk>
2022-05-08 06:40:30 +0530

    Lambda authorizer

---
+++ authorizer.js
@@ -0,0 +1,42 @@
+// Help function to generate an IAM policy
+var generatePolicy = (principalId, effect, resource) => {
+  var authResponse = {};
+
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = "2012-10-17";
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = "execute-api:Invoke";
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+
+  // Optional output with custom properties of the String, Number or Boolean type.
+  authResponse.context = {
+    stringKey: "stringval",
+    numberKey: 123,
+    booleanKey: true,
+  };
+  console.log(JSON.stringify(authResponse));
+  return authResponse;
+};
+exports.handler = (event, context, callback) => {
+  var token = event.authorizationToken;
+  switch (token) {
+    case "allow":
+      callback(null, generatePolicy("user", "Allow", event.methodArn));
+      break;
+    case "deny":
+      callback(null, generatePolicy("user", "Deny", event.methodArn));
+      break;
+    case "unauthorized":
+      callback("Unauthorized"); // Return a 401 Unauthorized response
+      break;
+    default:
+      callback("Error: Invalid token"); // Return a 500 Invalid token response
+  }
+};
