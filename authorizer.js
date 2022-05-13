b4e1ed3573b2bbee97e13a8f08b3c56903af88dd
Manoj Fernando <manojf@99x.lk>
2022-05-13 23:37:53 +0530

    Lambda authorizer

--- authorizer.js
+++ authorizer.js
@@ -1,3 +1,11 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: "USER_POOL_ID",
+  tokenUse: "id",
+  clientId: "CLIENT_ID"
+})
+
 const generatePolicy = (principalId, effect, resource) => {
   var authReponse = {};
   authReponse.principalId = principalId;
@@ -21,17 +29,16 @@ const generatePolicy = (principalId, effect, resource) => {
   return authReponse;
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   // lambda authorizer code
-  var token = event.authorizationToken; // "allow" or "deny"
-  switch (token) {
-    case "allow":
-      callback(null, generatePolicy("user", "Allow", event.methodArn));
-      break;
-    case "deny":
-        callback(null, generatePolicy("user", "Deny", event.methodArn));
-        break;
-    default: 
-        callback("Error: Invalid token");
+  var token = event.authorizationToken;
+  console.log(token);
+  // Validate the token
+  try {
+    const payload = await jwtVerifier.verify(token);
+    console.log(JSON.stringify(payload));
+    callback(null, generatePolicy("user", "Allow", event.methodArn));
+  } catch(err) {
+    callback("Error: Invalid token");
   }
 };
