9ded542c39da97cf76994a221527dc417c1eb0ac
Manoj Fernando <manojf@99x.lk>
2022-05-22 13:20:53 +0530

    Removed console.log

--- handler.js
+++ handler.js
@@ -69,7 +69,6 @@ module.exports.deleteNote = async (event, context, cb) => {
 };
 
 module.exports.getAllNotes = async (event, context, cb) => {
-  console.log(JSON.stringify(event));
   try {
     const params = {
       TableName: NOTES_TABLE_NAME,
